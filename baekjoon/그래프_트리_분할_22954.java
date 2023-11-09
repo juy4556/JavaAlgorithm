2378a514b85a138050a057c99c2f68c5840ffbfc
juy4556 <wnstjrtmdgus@gmail.com>
2023-11-09 23:30:01 +0900

    2023.11.09

---
+++ baekjoon/그래프_트리_분할_22954.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class 그래프_트리_분할_22954 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int u, v;
+        int[][] edges = new int[M+1][2];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            u = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+            edges[i] = new int[]{u, v};
+        }
+
+
+    }
+}
