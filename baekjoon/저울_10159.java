aa9def305496c5f6b27975b1c79045b3cc433a02
juy4556 <wnstjrtmdgus@gmail.com>
2024-02-28 23:30:01 +0900

    2024.02.28

---
+++ baekjoon/저울_10159.java
@@ -0,0 +1,47 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 저울_10159 {
+    static int N;
+    static int M;
+    static int[][] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        graph = new int[N + 1][N + 1];
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = 1;
+        }
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                    }
+                }
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            int count = 0;
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+                if (graph[i][j] == 0 && graph[j][i] == 0) {
+                    count++;
+                }
+            }
+            sb.append(count).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+}
