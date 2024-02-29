8316419cc234fd86e7109c011b1b542c03fbee74
juy4556 <wnstjrtmdgus@gmail.com>
2024-02-29 23:30:06 +0900

    2024.02.29

--- baekjoon/깊콘이_넘쳐흘러_17420.java
+++ baekjoon/깊콘이_넘쳐흘러_17420.java
@@ -3,9 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class 깊콘이_넘쳐흘러_17420 {
     public static void main(String[] args) throws IOException {
@@ -13,29 +11,44 @@ public class 깊콘이_넘쳐흘러_17420 {
         StringTokenizer st;
         long result = 0;
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        int[][] B = new int[N][2];
+        List<List<Integer>> gifticons = new ArrayList<>(N);
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            gifticons.add(new ArrayList<>());
+            gifticons.get(i).add(Integer.parseInt(st.nextToken()));
         }
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             int n = Integer.parseInt(st.nextToken());
-            B[i] = new int[]{n, i};
+            gifticons.get(i).add(n);
         }
-        Arrays.sort(B, Comparator.comparingInt(o -> o[0]));
-        int index = 0;
-        while (index < N) {
-            int useDay = B[index][0];
-            int leftDay = A[B[index][1]];
-            if (leftDay - useDay >= 0) {
-                index++;
-                continue;
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>(
+                Comparator.comparing((List<Integer> list) -> list.get(1)).thenComparingInt(list -> list.get(0)));
+        for (int i = 0; i < N; i++) {
+            pq.add(gifticons.get(i));
+        }
+//    gifticons.sort(Comparator.comparing((List<Integer> list) -> list.get(1)).thenComparingInt(list -> list.get(0)));
+        double lastDay = pq.peek().get(1);
+        double lastMaxDay = pq.peek().get(1);
+        double curMaxDay = 0;
+        while (!pq.isEmpty()) {
+            List<Integer> gifticon = pq.poll();
+            double deadLine = gifticon.get(0);
+            double planDay = gifticon.get(1);
+            if (lastDay < planDay) {
+                lastDay = planDay;
+                lastMaxDay = curMaxDay;
             }
-            result += (useDay - leftDay) / 30 + 1;
-            index++;
+
+            if (deadLine < lastMaxDay || deadLine < planDay) {
+                double maxDay = Math.max(lastMaxDay, planDay);
+                long extendCount = (long) Math.ceil((maxDay - deadLine) / 30);
+                deadLine += extendCount * 30;
+                result += extendCount;
+            }
+            curMaxDay = Math.max(curMaxDay, deadLine);
         }
+
         System.out.println(result);
     }
 }
