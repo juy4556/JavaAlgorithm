8cd6f92ff09ffec50b3e231f06af5b13b6d4e5d0
juy4556 <wnstjrtmdgus@gmail.com>
2024-03-05 23:30:01 +0900

    2024.03.05

---
+++ baekjoon/미네랄2_18500.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 미네랄2_18500 {
+    static int R, C;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        R = Integer.parseInt(br.readLine());
+        C = Integer.parseInt(br.readLine());
+
+    }
+}
