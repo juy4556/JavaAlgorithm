fea072ac8682b2d17bdfb15821f37d39135cd426
juy4556 <wnstjrtmdgus@gmail.com>
2024-03-11 20:41:37 +0900

    2024.03.11

--- baekjoon/괄호추가하기2_16638.java
+++ baekjoon/괄호추가하기2_16638.java
@@ -3,86 +3,87 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class 괄호추가하기2_16638 {
     static class Node {
         int value;
         char operator;
-        Node left;
-        Node right;
 
-        public Node(int value, char operator, Node left, Node right) {
+        public Node(int value, char operator) {
             this.value = value;
             this.operator = operator;
-            this.left = left;
-            this.right = right;
         }
     }
 
     static int N;
     static int result = Integer.MIN_VALUE;
-    static Node[] nodes;
-    static char[] operators;
-    static int[] visited;
-    static Queue<> queue;
+    static List<Node> nodes;
     static int operatorCount;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         operatorCount = N / 2;
-        nodes = new Node[operatorCount + 1];
+        nodes = new ArrayList<>();
         String expression = br.readLine();
-        Node head = new Node(expression.charAt(0) - '0', ' ', null, null);
-        nodes[0] = head;
-        initNodes(expression, head, 1);
-        dfs(0, new ArrayList<>());
+        Node head = new Node(expression.charAt(0) - '0', '+');
+        nodes.add(head);
+        if (N > 1) {
+            initNodes(expression, 1);
+        }
+        dfs(0, new HashSet<>());
         System.out.println(result);
     }
 
-    public static void initNodes(String expression, Node now, int index) {
-        Node node = new Node(expression.charAt(index + 1) - '0', expression.charAt(index), now, null);
-        nodes[index / 2 + 1] = node;
-        now.right = node;
+    public static void initNodes(String expression, int index) {
+        Node node = new Node(expression.charAt(index + 1) - '0', expression.charAt(index));
+        nodes.add(node);
         if (index + 2 < N) {
-            initNodes(expression, node, index + 2);
+            initNodes(expression, index + 2);
         }
     }
 
-    public static void dfs(int start, List<Integer> list) {
-        if ((list.size() & 1) == 0) {
-            int sum = 0;
-
-            for (int i = 0; i < list.size(); i += 2) {
-                int n1 = list.get(i);
-                int n2 = list.get(i + 1);
-                Node node1 = nodes[n1];
-                Node node2 = nodes[n2];
-
-                int temp = nodes[n1].value;
-                int left = n1;
-                int right = n2;
-                while (left < right) {
-                    temp = calculate(left, left + 1, nodes[n1 + 1].operator);
-                    left += 1;
+    public static void dfs(int start, Set<Integer> indexSet) {
+        int sum = 0;
+        Stack<Node> stack = new Stack<>();
+        for (int i = 0; i < nodes.size(); i++) {
+            Node node = nodes.get(i);
+            if (indexSet.contains(i)) {
+                Node node2 = nodes.get(i + 1);
+                int temp = calculate(node.value, node2.value, node2.operator);
+                if (node.operator == '*') {
+                    Node popped = stack.pop();
+                    popped.value = calculate(popped.value, temp, node.operator);
+                    stack.add(popped);
+                } else {
+                    stack.add(new Node(temp, node.operator));
                 }
-
-                char operator = operators[i / 2];
-                sum = calculate(sum, list.get(i), operator);
+                i++;
+            } else {
+                if (node.operator == '*') {
+                    Node popped = stack.pop();
+                    popped.value = calculate(popped.value, node.value, node.operator);
+                    stack.add(popped);
+                } else {
+                    stack.add(new Node(node.value, node.operator));
+                }
             }
-            result = Math.max(result, sum);
         }
-        for (int i = start; i < N / 2 + 1; i++) {
-            if (visited[i] == 0) {
-                visited[i] = 1;
-                list.add(i);
-                dfs(i + 2, list);
-                list.remove(list.size() - 1);
-                visited[i] = 0;
 
+        while (!stack.isEmpty()) {
+            Node popped = stack.pop();
+            if (popped.operator == '+') {
+                sum += popped.value;
+            } else {
+                sum -= popped.value;
             }
+        }
+        result = Math.max(result, sum);
+        for (int i = start; i < operatorCount; i++) {
+            indexSet.add(i);
+            dfs(i + 2, indexSet);
+            indexSet.remove(i);
         }
     }
 
@@ -100,3 +101,4 @@ public class 괄호추가하기2_16638 {
         }
     }
 }
+
