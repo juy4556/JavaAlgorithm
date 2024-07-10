1a5c96f7e6d1ca2a56aca5edaac694dc3c873063
juy4556 <wnstjrtmdgus@gmail.com>
2024-07-10 23:42:25 +0900

    2024.07.10

--- codetree/거스름돈계산하기2.java
+++ codetree/거스름돈계산하기2.java
@@ -3,7 +3,6 @@ package codetree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
