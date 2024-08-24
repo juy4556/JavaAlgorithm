7e19e8ec8bfb48ef1b4c2f1a4bb372203bdeb688
juy4556 <wnstjrtmdgus@gmail.com>
2024-08-24 23:30:01 +0900

    2024.08.24

---
+++ baekjoon/나만안되는연애.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 나만안되는연애 {
+    static int N, M;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+    }
+}
