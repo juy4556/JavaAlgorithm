bcf667ca5967cf8287ee0ff48f3b7bcdd130826d
juy4556 <wnstjrtmdgus@gmail.com>
2024-11-22 23:30:00 +0900

    2024.11.22

---
+++ programmers/하나둘셋떨어뜨리기.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class 하나둘셋떨어뜨리기 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] solution(int[][] edges, int[] target) {
+        int[] answer = {};
+        return answer;
+    }
+}
